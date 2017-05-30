3d05152dbe075dac9b5aed11d74de67c299e1821
Maia <maia.mcc@gmail.com>
2017-05-29 23:13:31 -0400

    remove background page/scripts

--- background.js
+++
@@ -1,7 +0,0 @@
-// chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-//   var resp = {};
-//   for (var key in localStorage) {
-//     a[key]=localStorage[key];
-//   }
-//   sendResponse(resp);
-// });
